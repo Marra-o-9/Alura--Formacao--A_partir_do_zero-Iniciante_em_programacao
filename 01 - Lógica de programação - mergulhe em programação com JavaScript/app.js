ff570393a368ddfbd01c7358a38adb29c60e03d5
Marra_o_9 <marrabarbosa9@gmail.com>
2024-04-17 19:59:35 -0300

    new file:   "01 - L\303\263gica de programa\303\247\303\243o - mergulhe em programa\303\247\303\243o com JavaScript/app.js" 	new file:   "01 - L\303\263gica de programa\303\247\303\243o - mergulhe em programa\303\247\303\243o com JavaScript/img/JS Game.html" 	new file:   "01 - L\303\263gica de programa\303\247\303\243o - mergulhe em programa\303\247\303\243o com JavaScript/img/JS Game_files/app.js" 	new file:   "01 - L\303\263gica de programa\303\247\303\243o - mergulhe em programa\303\247\303\243o com JavaScript/img/JS Game_files/style.css" 	new file:   "01 - L\303\263gica de programa\303\247\303\243o - mergulhe em programa\303\247\303\243o com JavaScript/img/Ruido.png" 	new file:   "01 - L\303\263gica de programa\303\247\303\243o - mergulhe em programa\303\247\303\243o com JavaScript/img/bg.png" 	new file:   "01 - L\303\263gica de programa\303\247\303\243o - mergulhe em programa\303\247\303\243o com JavaScript/img/code.png" 	new file:   "01 - L\303\263gica de programa\303\247\303\243o - mergulhe em programa\303\247\303\243o com JavaScript/img/robot.png" 	new file:   "01 - L\303\263gica de programa\303\247\303\243o - mergulhe em programa\303\247\303\243o com JavaScript/img/trophy.png" 	new file:   "01 - L\303\263gica de programa\303\247\303\243o - mergulhe em programa\303\247\303\243o com JavaScript/index.html" 	new file:   "01 - L\303\263gica de programa\303\247\303\243o - mergulhe em programa\303\247\303\243o com JavaScript/style.css" 	new file:   "02 - L\303\263gica de programa\303\247\303\243o - explore fun\303\247\303\265es e listas/app.js" 	new file:   "02 - L\303\263gica de programa\303\247\303\243o - explore fun\303\247\303\265es e listas/img/JS Game.html" 	new file:   "02 - L\303\263gica de programa\303\247\303\243o - explore fun\303\247\303\265es e listas/img/JS Game_files/app.js" 	new file:   "02 - L\303\263gica de programa\303\247\303\243o - explore fun\303\247\303\265es e listas/img/JS Game_files/style.css" 	new file:   "02 - L\303\263gica de programa\303\247\303\243o - explore fun\303\247\303\265es e listas/img/Ruido.png" 	new file:   "02 - L\303\263gica de programa\303\247\303\243o - explore fun\303\247\303\265es e listas/img/bg.png" 	new file:   "02 - L\303\263gica de programa\303\247\303\243o - explore fun\303\247\303\265es e listas/img/code.png" 	new file:   "02 - L\303\263gica de programa\303\247\303\243o - explore fun\303\247\303\265es e listas/img/ia.png" 	new file:   "02 - L\303\263gica de programa\303\247\303\243o - explore fun\303\247\303\265es e listas/index.html" 	new file:   "02 - L\303\263gica de programa\303\247\303\243o - explore fun\303\247\303\265es e listas/style.css"

---
+++ 01 - Lógica de programação - mergulhe em programação com JavaScript/app.js
@@ -0,0 +1,22 @@
+alert("Boas vindas ao jogo do número secreto!");
+let numeroMaximo = 100;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+let chute;
+let tentativas = 1;
+
+while (chute != numeroSecreto) {
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}:`);
+    if (chute == numeroSecreto) {
+        break;
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que o ${chute}.`);
+        } else {
+            alert(`O número secreto é maior que o ${chute}.`);
+        }
+        tentativas++;
+    }
+}
+
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+alert(`Certo! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}!`);
